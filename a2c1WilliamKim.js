e03beefbd10645830f63a331794b08c37ffce3ea
WilliamKim1edu <124187413+WilliamKim1edu@users.noreply.github.com>
2023-02-13 15:39:42 -0500

    Added additional comments to code
    https://editor.p5js.org/william.kim1/sketches/z4tIkUiSD

--- a2c1WilliamKim.js
+++ a2c1WilliamKim.js
@@ -7,20 +7,20 @@ function draw() {
   background(255);
   stroke("white");
   strokeWeight(1);
-//background
+//background creation
   gridBG(0)
   gridBG(80)
   gridBG(160)
   gridBG(240)
   gridBG(320)
-//letter
+//letter creation
   gridlet1(0)
   gridlet1(160)
   gridlet1(320)
   gridlet2(80)
   gridlet2(240)
 }
-
+//draws background
 function gridBG(col) {
   fill ("indigo")
   rect (col, 0, 80, 80);
@@ -29,7 +29,7 @@ function gridBG(col) {
   rect (col, 240, 80, 80);
   rect (col, 320, 80, 80);
 }
-
+//draws tall parts of W
 function gridlet1(col) {
   fill ("black")
   rect (col, 0, 80, 80);
@@ -37,7 +37,7 @@ function gridlet1(col) {
   rect (col, 160, 80, 80);
   rect (col, 240, 80, 80);
 }
-
+//draws low parts of W
 function gridlet2(col) {
   fill ("black")
   rect (col, 320, 80, 80);
